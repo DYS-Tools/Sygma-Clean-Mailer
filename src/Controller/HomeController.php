f39e4b8ee9a80445ddc14263a5cef38ae2f6f7aa
DurandSacha <sacha6623@gmail.com>
2020-04-09 19:42:41 +0200

    add email Form in Emailing section

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Form\SendMailType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends AbstractController
@@ -20,11 +21,28 @@ class HomeController extends AbstractController
     /**
      * @Route("/mail", name="mail")
      */
-    public function mail()
+    public function mail(Request $request, \Swift_Mailer $mailer)
     {
-        //$form = new SendMailType();
+        $form = $this->createForm(SendMailType::class);
+        $form->handleRequest($request);
+
+        $name = $request->request->get('Name');;
+        $email = $request->request->get('Email');
+        $object = $request->request->get('Subject');
+        $msg = $request->request->get('Message');
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $message = (new \Swift_Message($object))
+                ->setFrom($form->get($email)->getData())
+                ->setTo('sacha6623@gmail.com')
+                ->setBody($msg);
+
+            $mailer->send($message);
+
+            $this->addFlash('info', "Email has been send");
 
         return $this->render('mail.html.twig', [
+            'EmailForm' => $form->createView(),
         ]);
     }
 
--- src/Form/SendMailType.php
+++ src/Form/SendMailType.php
@@ -3,6 +3,10 @@
 namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -11,7 +15,28 @@ class SendMailType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('field_name')
+            ->add('Name', TextType::class, [
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Name'],
+                'label' => false,
+            ])
+            ->add('Email', EmailType::class, [
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Email'],
+                'label' => false,
+            ])
+            ->add('Subject', TextType::class, [
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Subject'],
+                'label' => false,
+            ])
+            ->add('Message', TextareaType::class, [
+                'attr' => ['class' => 'form-control', 'placeholder' => 'Message'],
+                'label' => false,
+
+            ])
+
+            ->add('Send', SubmitType::class, [
+                'attr' => ['type'=>'submit', 'class' => 'btn btn-success button-big button-rouded', ]
+            ])
+
         ;
     }
 
