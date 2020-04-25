f5a30b700835015815c73c6233e4e0463409efa3
DurandSacha <sacha6623@gmail.com>
2020-04-25 22:15:58 +0200

    [Feature] Change mailForm and add mailTemplate part

--- src/Form/sendMail/SendMailType.php
+++ src/Form/sendMail/SendMailType.php
@@ -2,7 +2,11 @@
 
 namespace App\Form\sendMail;
 
+use App\Entity\ListMail;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
@@ -12,6 +16,13 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class SendMailType extends AbstractType
 {
+    private $em;
+
+    public function __construct(EntityManagerInterface $em)
+    {
+        $this->em = $em;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
@@ -31,6 +42,13 @@ class SendMailType extends AbstractType
                 'attr' => ['class' => 'form-control', 'placeholder' => 'Message'],
                 'label' => false,
 
+            ])
+            ->add('list', EntityType::class, [
+                'class' => ListMail::class,
+                'label' => 'Envoyer a la liste',
+                'choice_label' => function(ListMail $category) {
+                    return sprintf('%s', $category->getName());
+                },
             ])
 
             ->add('Send', SubmitType::class, [
