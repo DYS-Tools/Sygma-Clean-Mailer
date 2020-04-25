d7ebc5306bb2b9201a59290002442bedee42f4e5
Yohann76 <yohanndurand76@gmail.com>
2020-04-25 16:06:59 +0200

    [SearchMail] add form for use the keyword

--- src/Controller/searchMail/SearchMailController.php
+++ src/Controller/searchMail/SearchMailController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller\searchMail;
 
+use App\Form\searchMail\KeywordSearchType;
 use App\Service\searchMail\SearchMailGoogle;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SearchMailController extends AbstractController
@@ -11,16 +13,27 @@ class SearchMailController extends AbstractController
     /**
      * @Route("/search/mail", name="searchMail")
      */
-    public function index(SearchMailGoogle $searchMailGoogle)
+    public function index(SearchMailGoogle $searchMailGoogle, Request $request)
     {
         // Todo : form with keyword
         // Todo use SearchInGoogleWithKeyword Service
 
-        $mailGoogle = $searchMailGoogle->SearchInGoogle();
-        dd($mailGoogle);
+        $form = $this->createForm(KeywordSearchType::class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $keyword1 = $form['keyword']->getData();
+
+
+            $mailGoogle = $searchMailGoogle->SearchInGoogleLink($keyword1);
+
+        }
+
+        //$mailGoogle = $searchMailGoogle->SearchInGoogleLink();
+        //dd($mailGoogle);
 
         return $this->render('searchMail/index.html.twig', [
-            'controller_name' => 'SearchMailController',
+            'form' => $form->createView()
         ]);
     }
 }
